bb052980cae3fd10a85fd38ff46bffc2f4f9fd3e
Manikaaggarwal24 <angel24592@gmail.com>
2024-03-03 18:20:14 -0500

    thr

--- src/components/AuthScreens/ForgotPasswordScreen.js
+++ src/components/AuthScreens/ForgotPasswordScreen.js
@@ -13,7 +13,7 @@ const ForgotPasswordScreen = () => {
 
     try {
       const { data } = await axios.post(
-        "/auth/forgotpassword",
+        "https://blog2-backend-api.onrender.com/auth/forgotpassword",
         { email }
       );
 
--- src/components/AuthScreens/ResetPasswordScreen.js
+++ src/components/AuthScreens/ResetPasswordScreen.js
@@ -25,7 +25,7 @@ const ResetPasswordScreen = () => {
 
     try {
       const { data } = await axios.put(
-        `/auth/resetpassword?resetPasswordToken=${token}`,
+        `https://blog2-backend-api.onrender.com/auth/resetpassword?resetPasswordToken=${token}`,
         {
           password,
         }
